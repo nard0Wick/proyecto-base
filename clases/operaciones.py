ca2b84248ad8cbfa7646c0e6ff7fb0c51e527eea
Carlos Chavez <charlyccr4@gmail.com>
2025-09-18 18:22:33 -0600

    Version 1 -  feature/multiplicacion 18/09/2025 CORRECCION

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -23,6 +23,9 @@ class Operaciones:
     def sumar(self):
         self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igula a " + str(self.num1 + self.num2)
     
+    def multiplicacion(self):
+        self.resultado = "La multiplicacion de " + str(self.num1) + " * " + str(self.num2) + " es igula a " + str(self.num1 * self.num2)
+    
     def mostrarResultado(self):
         print(self.resultado)
         
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
     op.leerNumeros()
     op.sumar()
     op.mostrarResultado()
-    
+    op.multiplicacion()
+    op.mostrarResultado()
 if __name__ == "__main__":
     main()
     
